8af82a407564d41c03921e6336ec5a8f40d17e6e
Joshua David <cxang.2022@scis.smu.edu.sg>
2024-01-16 09:34:09 +0800

    fix: forward ref ui component warning

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -6,19 +6,20 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, name, ...props }) => {
+  ({ className, type, name, ...props }, ref) => {
     return (
       <input
         type={type}
+        ref={ref}
         className={cn(
           "flex h-12 w-full rounded-md bg-gray-100 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none  disabled:cursor-not-allowed disabled:opacity-50",
-          className,
+          className
         )}
         name={name}
         {...props}
       />
     );
-  },
+  }
 );
 Input.displayName = "Input";
 
